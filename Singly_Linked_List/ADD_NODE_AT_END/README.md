281f017ea7d0d9cc153d2dc94088381d5616cb8f
mehulshah225 <mehulshah2225@gmail.com>
2023-10-20 02:32:21 -0500

    Add node at end

---
+++ Singly_Linked_List/ADD_NODE_AT_END/README.md
@@ -0,0 +1,10 @@
+# This is the README for addNodeAtEnd.c
+
+The program focuses to add a node at the end of the list
+
+Logic explained:
+
+-    Create a new node using malloc and createNode function.
+-    let the new nodes next be NULL
+-    iterate through while until it reaches NULL. 
+-    point last node to new node
---
+++ Singly_Linked_List/ADD_NODE_AT_END/addNodeAtEnd.c
@@ -0,0 +1,79 @@
+/* Code to add node ath the end of the list */
+/* Author: Mehul Shah */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{    
+        int data;
+        struct node* next;
+}node_t;
+
+node_t* head = NULL;
+
+// Function to create new node
+node_t* createNode(int data){
+    node_t* newNode = (node_t*)malloc(sizeof(node_t));
+    if(newNode == NULL)
+            printf("Malloc failed");
+
+    newNode->data = data;
+    newNode->next = NULL;
+    
+    return newNode;
+}
+
+//Function to insert nodes in the list
+void insertNode(int data){
+    node_t* new = createNode(data);
+    node_t* current = head;
+    if(new == NULL)
+            printf("Malloc failed");
+
+    new->next = current;
+    head = new;
+}
+
+// Fnction to insert nodes at the end of the list
+void insertAtEnd(int data){
+    node_t* current = head;
+    node_t* insertNode = createNode(data);
+
+    insertNode->data = data;
+    insertNode->next = NULL;
+    
+    if(head == NULL)
+        head = insertNode;
+        
+    //Loop until you reach the last element
+    while(current->next != NULL){
+        current = current->next;
+    }
+    
+    //Assign new node at the end
+    current->next=insertNode;
+}
+
+// Function to display the list
+void displayList(){
+    node_t* current = head;
+    while(current != NULL){
+            printf("%d ->", current->data);
+            current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main(void){
+
+    insertNode(4);
+    insertNode(6);
+    insertNode(7);
+    insertNode(11);
+    displayList();
+
+    insertAtEnd(10);
+    displayList();
+
+    return 0;
+}
